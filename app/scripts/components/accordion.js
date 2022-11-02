6ba2d7a54b852e8a304b86c271c8f202efeb8615
hyoon <haileyisworkinghere@gmail.com>
2022-11-02 17:35:45 -0400

    updated accordion

--- app/scripts/components/accordion.js
+++ app/scripts/components/accordion.js
@@ -1,19 +1,32 @@
 export default class Accordion {
   constructor(elem, APP) {
     this.elem = elem;
-    this.triggers = this.elem.querySelectorAll(".accordion-trigger");
-    this.panels = this.elem.querySelectorAll(".accordion-panel");
+    this.triggers = this.elem.querySelectorAll(".js-trigger");
+    this.panels = this.elem.querySelectorAll(".js-panel");
     this.allowMultiple = false;
   }
 
   togglePanel(index) {
     const trigger = this.triggers[index];
     const panel = this.panels[index];
-    const panelHeight = `${
-      panel.querySelector(".accordion-content").offsetHeight
-    }px`;
+    const panelHeight = `${panel.querySelector(".js-content").offsetHeight}px`;
 
-    panel.classList.add('expanded')
+    trigger.classList.toggle("expanded");
+    panel.classList.toggle("expanded");
+
+    if (panel.classList.contains("expanded")) {
+      panel.classList.remove("animate-out");
+      panel.classList.add("animate-in");
+      panel.style.height = panelHeight;
+      trigger.setAttribute("aria-expanded", true);
+      panel.setAttribute("aria-hidden", false);
+    } else {
+      panel.classList.remove("animate-in");
+      panel.classList.add("animate-out");
+      panel.style.height = 0;
+      trigger.setAttribute("aria-expanded", false);
+      panel.setAttribute("aria-hidden", true);
+    }
   }
 
   setTriggerHandler() {
@@ -36,19 +49,11 @@ export default class Accordion {
     });
   }
 
-  keyDownHandler(e) {
-
-  }
-
   init() {
     this.setTriggerHandler();
 
     window.addEventListener("resize", () => {
       this.resizeAccordion();
     });
-
-    this.elem.addEventListener("keydown", (e) => {
-      this.keyDownHandler(e);
-    })
   }
 }
